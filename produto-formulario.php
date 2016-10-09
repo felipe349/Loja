f2dbdf6cb87a1447a3d6ac93cf09b3133cc0a5b8
felipe349 <claiohm@gmail.com>
2016-10-09 15:45:37 -0300

    Add files via upload

--- produto-formulario.php
+++ produto-formulario.php
@@ -1,8 +1,22 @@
 <?php include("cabecalho.php"); ?>
 	<h1>Formulario de produto</h1>
+
 	<form action="adiciona-produto.php">
-		Nome: <input type="text" name="nome"><br/>
-		Preco: <input type="text" name="preco"><br/>
-		<input type="submit" value="Cadastrar">
+		<table class="table">
+			<tr>
+				<td>Nome:</td> 
+				<td><input class="form-control" type="text" name="nome"><br/></td>
+			</tr>
+
+			<tr>
+				<td>Preco:</td>
+				<td><input class="form-control" type="number" name="preco"><br/></td>
+			</tr>
+
+			<tr>
+				<td><button class="btn btn-primary" type="submit">Cadastrar</button></td>
+			</tr>
+
+		</table>
 	</form>
 <?php include("rodape.php"); ?>
